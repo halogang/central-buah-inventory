52a948516fc788ee33864b2a79c1deb162de77c8
R230905 <afrizalfajri23@gmail.com>
2026-03-12 12:15:28 +0700

    buiild

---
+++ public/build/assets/arrow-up-right-B7crr7vr.js
@@ -0,0 +1,1 @@
+import{c as o}from"./createLucideIcon-BVfVwawb.js";const t=[["path",{d:"M17 7 7 17",key:"15tmo1"}],["path",{d:"M17 17H7V7",key:"1org7z"}]],a=o("ArrowDownLeft",t);const r=[["path",{d:"M7 7h10v10",key:"1tivn9"}],["path",{d:"M7 17 17 7",key:"1vkiza"}]],c=o("ArrowUpRight",r);export{a as A,c as a};
