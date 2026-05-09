0434779758e968f96880ac944be340bb102f9490
R230905 <afrizalfajri23@gmail.com>
2026-05-09 18:58:17 +0700

    export url

--- resources/views/pdf/delivery-order.blade.php
+++ resources/views/pdf/delivery-order.blade.php
@@ -227,10 +227,10 @@
                     Pengirim
                     <br><br>
 
-                    @if($senderSignature)
+                    @if($deliveryOrder->sender_signature)
                         <img
-                            src="{{ $senderSignature }}"
-                            alt="{{ $senderSignature }}"
+                            src="{{ public_path($deliveryOrder->sender_signature) }}"
+                            alt="TTD Pengirim"
                             height="60"
                         >
                     @endif
@@ -243,9 +243,9 @@
                     Penerima
                     <br><br>
 
-                    @if($receiverSignature)
+                    @if($deliveryOrder->receiver_signature)
                         <img
-                            src="{{ $receiverSignature }}"
+                            src="{{ public_path($deliveryOrder->receiver_signature) }}"
                             alt="TTD Penerima"
                             height="60"
                         >
